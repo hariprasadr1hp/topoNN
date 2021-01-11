4861b8d44c64596070b9dfe3d96041acdb9b1604
hariprasadr1hp <hariprasadr1hp@gmail.com>
2021-01-11 21:06:30 +0100

    Material Routine for an isotropic material

---
+++ main.py
@@ -0,0 +1,8 @@
+from pythonlib import meshGen
+
+
+mesh = meshGen.Plate(4,1,2)
+
+
+print(mesh.elemNodes)
+print(mesh.nodeCoords)
---
+++ pythonlib/materialRoutine.py
@@ -0,0 +1,33 @@
+"""
+libMat: Defines the material routine
+"""
+import numpy as np
+
+class matGen:
+    def __init__(self,matlParams,eps,del_eps=0):
+        self.ymod = matlParams[0]
+        self.Nu = matlParams[1]
+        self.sigYield = matlParams[2]
+        self.eps = eps
+        self.del_eps = del_eps
+
+    def LEIsotropic(self):
+        """
+        Returns Stress and Material Stiffness matrix
+        for Linear-Elastic Isotopic Material
+        """
+        const = self.ymod / ( (1+self.Nu) * (1-(2*self.Nu)) )
+        a = const * self.Nu
+        b = const * (1-self.Nu)
+        c = (a-b)/2
+        Cmat = np.array(
+            [
+                [b, a, a, 0, 0, 0],
+                [a, b, a, 0, 0, 0],
+                [a, a, b, 0, 0, 0],
+                [0, 0, 0, c, 0, 0],
+                [0, 0, 0, 0, c, 0],
+                [0, 0, 0, 0, 0, c]
+            ],dtype=float)
+        stress_el = Cmat @ self.eps
+        return stress_el, Cmat
